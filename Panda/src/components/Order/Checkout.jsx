a5e62d34256e7e73e814727246fe55dadf06d036
manalizo <yumimanal8@gmail.com>
2025-01-09 00:21:30 +0100

    add image to checkout

--- Panda/src/components/Order/Checkout.jsx
+++ Panda/src/components/Order/Checkout.jsx
@@ -1,16 +1,10 @@
-import CommandeForm from "./CommandeForm"
 import React, { useEffect, useState } from "react"
-import {
-	FaBox,
-	FaTruck,
-	FaMoneyBillAlt,
-	FaTshirt
-} from "react-icons/fa"
-
+import { FaBox, FaTruck, FaMoneyBillAlt, FaTshirt } from "react-icons/fa"
 import { useParams } from "react-router-dom"
-import { getCommandeById } from "../utils/ApiFunctions"
+import { getProductById, getProductImageById } from "../utils/ApiFunctions"
+import CommandeForm from "./CommandeForm"
 import ProductCarousel from "../common/ProductCarousel"
-import {getProductById} from "../utils/ApiFunctions"
+
 const Checkout = () => {
 	const [error, setError] = useState(null)
 	const [isLoading, setIsLoading] = useState(true)
@@ -20,21 +14,31 @@ const Checkout = () => {
 		prix: "",
 		description: "",
 	})
-
 	const { productId } = useParams()
 
 	useEffect(() => {
-		setTimeout(() => {
-			getProductById(productId)
-				.then((response) => {
-					setProductInfo(response)
-					setIsLoading(false)
+		const fetchProductData = async () => {
+			try {
+				// Fetch product information
+				const productResponse = await getProductById(productId)
+
+				// Fetch product image
+				const productImage = await getProductImageById(productId)
+
+				// Update state with product info and image
+				setProductInfo({
+					...productResponse,
+					image: productImage,
 				})
-				.catch((error) => {
-					setError(error)
-					setIsLoading(false)
-				})
-		}, 1000)
+				setIsLoading(false)
+			} catch (error) {
+				setError(error.message)
+				setIsLoading(false)
+			}
+		}
+
+		// Call the function to fetch data
+		fetchProductData()
 	}, [productId])
 
 	return (
@@ -49,7 +53,7 @@ const Checkout = () => {
 						) : (
 							<div className="room-info">
 								<img
-									src={`data:image/png;base64,${productInfo.image}`}
+									src={`data:image/png;base64,${productInfo.image}`} // Display the image after it's fetched
 									alt="Product photo"
 									style={{ width: "100%", height: "200px" }}
 								/>
@@ -67,7 +71,6 @@ const Checkout = () => {
 											<th>Description:</th>
 											<td>{productInfo.description}</td>
 										</tr>
-									
 										<tr>
 											<th>Shipping Info:</th>
 											<td>
@@ -95,10 +98,10 @@ const Checkout = () => {
 				</div>
 			</section>
 			<div className="container">
-				  <ProductCarousel/> 
-			
+				<ProductCarousel />
 			</div>
 		</div>
 	)
 }
+
 export default Checkout
